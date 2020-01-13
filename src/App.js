7bb915c1908a852f499b16fb074d9c4b3fe6cc37
Nikolas Santis <nikosantis@gmail.com>
2020-01-13 18:31:53 -0300

    componentWillMount

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react'
+import ComponentWillMount from './sections/life-cycle/componentWillMount'
 
 class App extends Component {
-  // constructor por defecto
-  // constructor (...args) {
-  //   super(...args)
-  // }
 
-  constructor (props) {
-    console.log('constructor')
-    super(props) // este método llama al constructor de Component
-    // inicializamos el state de nuestro component
-    this.state = { mensajeInicial: 'mensaje inicial' }
-    // bindeamos el contexto del método
-    // this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick = () => {
-    this.setState({ mensajeInicial: 'mensaje cambiado' })
-  }
   render () {
-    console.log('render')
     return (
       <div className="App">
-        <h4>Ciclo de montaje: contructor</h4>
-        {this.state.mensajeInicial}
-        <button onClick={this.handleClick}>
-          Cambiar mensaje
-        </button>
+        <ComponentWillMount />
       </div>
     )
   }
---
+++ src/sections/life-cycle/componentWillMount.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class ComponentWillMount extends Component {
+  constructor (props) {
+    console.log('constructor')
+    super(props)
+    this.state = { mensaje: 'mensaje inicial' }
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+    this.setState({ mensaje: 'mensaje modificado' })
+  }
+
+  render () {
+    console.log('render')
+    return (
+      <div>
+        <h4>Ciclo de montaje: Component Will Mount</h4>
+        <p>{this.state.mensaje}</p>
+      </div>
+    )
+  }
+}
+
+export default ComponentWillMount

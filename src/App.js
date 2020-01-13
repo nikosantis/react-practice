918981d4e7310187b1fddad9ba667d890b7d8769
Nikolas Santis <nikosantis@gmail.com>
2020-01-13 15:57:45 -0300

    click event

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react'
-import cars from './data/cars.json'
-
-class CarItem extends Component {
-  render () {
-    const { car } = this.props
-    return (
-      <li>
-        <p><strong>Nombre:</strong>{car.name}</p>
-        <p><strong>Marca:</strong>{car.company}</p>
-      </li>
-    )
-  }
-}
 
 class App extends Component {
   render () {
 
     return (
       <div className="App">
-        <h4>Trabajando con listas con Objeto</h4>
-        <ul>
-          {
-            cars.map(car => {
-              return (
-                <CarItem key={car.id} car={car} />
-              )
-            })
-          }
-        </ul>
+        <h4>Eventos</h4>
+        <button onClick={() => alert('Hi there!')}>Hi there!</button>
       </div>
     )
   }

ccdf7c8028ad577578b7ed32d4e5a1fbfccf8627
Nikolas Santis <nikosantis@gmail.com>
2020-01-13 12:49:36 -0300

    default props

--- src/App.js
+++ src/App.js
@@ -2,66 +2,23 @@ import React, {Component} from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-// const Hello = props => {
-//   return <h2>{props.title}</h2>
-// }
-
-class Hello extends Component {
-  render() {
-    return <h2>{this.props.title}</h2>
+class Title extends Component {
+  render () {
+    return <h1>{this.props.text}</h1>
   }
 }
 
-class Text extends Component {
-  render() {
-    const {
-      isActivated,
-      arrayOfNumbers,
-      multiply,
-      text,
-      number,
-      objectWithInfo,
-      title
-    } = this.props
-
-    const textSegunBool = isActivated ? 'On' : 'Off'
-    const mappedNumbers = arrayOfNumbers.map(multiply)
-
-    return (
-      <>
-        {title}
-        <p>{text}</p>
-        <p>{number}</p>
-        <p>{mappedNumbers}</p>
-        <p>{textSegunBool}</p>
-        <p>{objectWithInfo.key}</p>
-      </>
-    )
-  }
+Title.defaultProps = {
+  text: 'Título por defecto'
 }
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Hello title='Hello Niko' />
-          <Text
-            arrayOfNumbers={[2, 3, 10]}
-            objectWithInfo={{ key: 'First Value', key2: 'otherValue' }}
-            isActivated
-            multiply={(number) => number * 4}
-            number={2}
-            text='Texto en String'
-            title={<h1>Esto es un H1</h1>}
-          />
-        </header>
+        <Title text='Otro título desde props' />
       </div>
-    );
+    )
   }
 }
 

c7d7a04ad1ca21293888ab1e4ae20beab5056a0a
Nikolas Santis <nikosantis@gmail.com>
2020-01-13 16:33:44 -0300

    form in react

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, { Component } from 'react'
+import Forms from './sections/forms'
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = { mouseX: 0, mouseY: 0 }
-  }
-
-  handleMouseMove = (e) =>  {
-    const { clientX, clientY } = e
-    this.setState({ mouseX: clientX, mouseY: clientY })
-  }
-  handleClick (e) {
-    console.log(e)
-    console.log(e.nativeEvent)
-    alert('Hi there!')
-  }
   render () {
-
     return (
       <div className="App">
-        <h4>Eventos</h4>
-        <button onClick={this.handleClick}>Hi there!</button>
-        <div
-          onMouseMove={this.handleMouseMove}
-          style={{ border: '1px solid #000', marginTop: 10, padding: 10 }}
-        >
-          <p>{this.state.mouseX}, {this.state.mouseY}</p>
-        </div>
+        <Forms />
       </div>
     )
   }
---
+++ src/sections/forms.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class Forms extends Component {
+  handleClick (e) {
+    e.preventDefault()
+    const name = document.getElementById('name').value
+    const email = document.getElementById('twitter').value
+    console.log({ name, email })
+  }
+  render() {
+    return (
+      <div>
+        <h4>Formularios</h4>
+        <form>
+          <p>
+            <label>Nombre: </label>
+            <input
+              id='name'
+              name='userName'
+              placeholder='Introduce el nombre'
+            />
+          </p>
+          <p>
+            <label>Twitter: </label>
+            <input
+              id='twitter'
+              name='twitteraccount'
+              placeholder='Introduce tu Twitter'
+            />
+          </p>
+
+          <button onClick={this.handleClick}>Enviar</button>
+        </form>
+      </div>
+    )
+  }
+}

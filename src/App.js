dcbea40bc54e2ada5ecdbc85377878745b68e808
Nikolas Santis <nikosantis@gmail.com>
2020-01-15 17:38:20 -0300

    composition and heritage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
-import EjemploDeComponentDidCatch from './sections/life-cycle/componentDidCatch.js'
+
+class Button extends Component {
+
+  render () {
+    return (
+      <button style={{ borderColor: this.props.borderColor, display: 'block' }}>
+        {this.props.label}
+      </button>
+    )
+  }
+}
+
+Button.defaultProps = {
+  borderColor: '#09f'
+}
+
+class ButtonDanger extends Component {
+  render () {
+    return <Button borderColor='red' label={this.props.label} />
+  }
+}
+
+class ButtonWithLegend extends Component {
+  render () {
+    return (
+      <div>
+        <Button label={this.props.label} borderColor={this.props.borderColor} />
+        <small>{this.props.legend}</small>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <EjemploDeComponentDidCatch />
+        <h4>Composición vs. herencia</h4>
+        <Button label='Click aquí con composición' />
+        <br/>
+        <ButtonDanger label='Cuidado con composición!!' />
+        <br/>
+        <ButtonWithLegend label='Botón de explicación con composición' legend='Click a el botón para hacer algo' />
       </div>
     )
   }

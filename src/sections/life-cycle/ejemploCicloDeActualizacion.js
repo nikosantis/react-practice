f9f304820006fc3e0321a36264c554b236855574
Nikolas Santis <nikosantis@gmail.com>
2020-01-15 13:00:55 -0300

    componentWillUpdate

--- src/sections/life-cycle/ejemploCicloDeActualizacion.js
+++ src/sections/life-cycle/ejemploCicloDeActualizacion.js
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 const ANIMAL_IMAGES = {
@@ -9,13 +9,34 @@ const ANIMAL_IMAGES = {
 
 const ANIMALS = Object.keys(ANIMAL_IMAGES)
 
-class AnimalImage extends PureComponent {
+class AnimalImage extends Component {
   state = { src: ANIMAL_IMAGES[this.props.animal] }
 
   componentWillReceiveProps (nextProps) {
+    console.clear()
     console.log('1. componentWillReceiveProps')
     console.log(nextProps)
     this.setState({ src: ANIMAL_IMAGES[nextProps.animal] })
+  }
+
+  shouldComponentUpdate (nextProps) {
+    console.log('2. shouldComponentUpdate', nextProps)
+    return this.props.animal !== nextProps.animal
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+    console.log('3. componentWillUpdate', nextProps, nextState)
+    const img = document.querySelector('img')
+    console.log('from img element', { alt: img.alt })
+    // web animation api
+    img.animate([ {
+      filter: 'blur(0px)'
+    }, {
+      filter: 'blur(2px)'
+    } ], {
+      duration: 500,
+      easing: 'ease'
+    })
   }
 
   render () {
